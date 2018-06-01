12cc9b6d071ac7843f701fa42ec33b42a1bb58b3
Mike McNally <mike@zeamster.com>
2018-06-01 17:16:59 -0400

    Started combat loop

--- src/combat.js
+++ src/combat.js
@@ -1,5 +1,12 @@
-const start_combat = button_pressed.pipe(filter(button => button === 'Start Combat'));
+const start_combat = button_pressed.pipe(filter(button => button === 'Start Combat'), share());
 const end_combat = start_combat.pipe(
-  switchMapTo(button_pressed.pipe(filter(button => button === 'End Combat')))
+  switchMapTo(button_pressed.pipe(filter(button => button === 'End Combat')), share())
 );
 
+const combat_phase = prompt_character_actions.pipe(
+  concatAll()
+);
+
+const combat_round = start_combat.pipe(switchMapTo(
+
+))
