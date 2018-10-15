4815d70bf226bf94adbfa9360b4653446d335300
mikermcnally <mikermcnally@gmail.com>
2018-10-14 22:40:06 -0400

    Fixed attribute mods

--- src/mml/character/attribute_mods.ts
+++ src/mml/character/attribute_mods.ts
@@ -1,10 +1,10 @@
-import { Character } from "./character";
+import { Integer } from "../../utilities/integer";
 
-export const Strength: ReadonlyArray<Character['strength']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Coordination: ReadonlyArray<Character['coordination']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Beauty: ReadonlyArray<Character['beauty']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Intellect: ReadonlyArray<Character['intellect']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Reason: ReadonlyArray<Character['reason']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Creativity: ReadonlyArray<Character['creativity']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Presence: ReadonlyArray<Character['presence']> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
-export const Perception: ReadonlyArray<Character['perception']> = [-10, -10, -10, -10, -10, -10, -10, -10, -5, -5, 0, 0, 3, 3, 5, 5, 8, 8, 10, 10, 15, 15, 15, 20];
+export const Strength: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Coordination: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Beauty: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Intellect: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Reason: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Creativity: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Presence: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -5, -3, -3, 0, 0, 3, 3, 3, 5, 5, 5, 8, 8, 8, 10, 10, 15];
+export const Perception: ReadonlyArray<Integer.Unsigned> = [-10, -10, -10, -10, -10, -10, -10, -10, -5, -5, 0, 0, 3, 3, 5, 5, 8, 8, 10, 10, 15, 15, 15, 20];
